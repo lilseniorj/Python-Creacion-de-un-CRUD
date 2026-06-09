e9f496097f1de6dcadedad613f022cbf24d28be6
lilseniorj <jesusvarguer18@gmail.com>
2026-06-09 16:54:17 -0500

    Funciones y scope global en Python

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-clients = 'Pablo, ricardo, '
+clients = 'Pablo, Ricardo, '
+
+def create_client(client_name):
+    global clients
+
+    clients += client_name
+    _add_comma()
+
+
+def list_clients():
+    global clients
+
+    print(clients)
+
+
+def _add_comma():
+    global clients
+
+    clients += ', '
+
 
 if __name__ == '__main__':
-    clients += 'maria'
-    print(clients)
+    list_clients()
+
+    create_client('Maria')
+
+    list_clients()
