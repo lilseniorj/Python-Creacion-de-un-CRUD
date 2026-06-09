765550c05d3739ec88cb5b4d40f2ea530a7ca946
lilseniorj <jesusvarguer18@gmail.com>
2026-06-09 17:53:56 -0500

    Refactorización Python: método auxiliar privado

--- main.py
+++ main.py
@@ -11,6 +11,15 @@ def create_client(client_name):
         print("Client already exists in the system")
 
 
+def update_client(client_name, update_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ", ", update_client_name + ", ")
+    else:
+        print("Client is not in the system")
+
+
 def list_clients():
     global clients
 
@@ -28,23 +37,38 @@ def _print_welcome():
     print("*" * 50)
     print("What would you like to do today?")
     print("[C]reate client")
+    print("[U]pdate client")
     print("[D]elete client")
+
+
+def _get_client_name():
+    client_name = None
+
+    while not client_name:
+        client_name = input("What is the name of the client? ")
+
+    return client_name
 
 
 if __name__ == "__main__":
     _print_welcome()
 
     command = input()
+    command = command.upper()
 
     if command == "C":
-        client_name = input("What is the name of the client? ")
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
     elif command == "D":
         pass
+    elif command == "U":
+        client_name = _get_client_name()
+        update_client_name = input("What is the new name of the client? ")
+        update_client(client_name, update_client_name)
+        list_clients()
     else:
         print("Invalid command")
-
 
     create_client("Maria")
 
