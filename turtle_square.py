f64af0aa4ce949147f9db90615a6cd4ee93f606e
lilseniorj <jesusvarguer18@gmail.com>
2026-06-09 15:26:12 -0500

    First commit

---
+++ turtle_square.py
@@ -0,0 +1,14 @@
+import turtle
+
+window = turtle.Screen()
+turtle = turtle.Turtle()
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+window.mainloop()
