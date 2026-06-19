f1145f2cc72acd85a95c130097164c75f1b17635
lilseniorj <jesusvarguer18@gmail.com>
2026-06-18 19:18:06 -0500

    Refactorizar codigo con listas en Python

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-clients = "Pablo, Ricardo, "
+clients = ["Pablo", "Ricardo"]
 
 
 def create_client(client_name):
     global clients
 
     if client_name not in clients:
-        clients += client_name
-        _add_comma()
+        clients.append(client_name)
     else:
         print("Client already exists in the system")
 
@@ -15,7 +14,8 @@ def update_client(client_name, update_client_name):
     global clients
 
     if client_name in clients:
-        clients = clients.replace(client_name + ", ", update_client_name + ", ")
+        index = clients.index(client_name)
+        clients[index] = update_client_name
     else:
         print("Client is not in the system")
 
@@ -24,31 +24,24 @@ def delete_client(client_name):
     global clients
 
     if client_name in clients:
-        clients = clients.replace(client_name + ", ", "")
+        clients.remove(client_name)
     else:
         print("Client is not in the system")
 
 
 def search_client(client_name):
-    clients_list = clients.split(", ")
 
-    for client in clients_list:
-        if client != client_name:
-            continue
-        else:
+    for client in clients:
+        if client.lower() == client_name.lower():
             return True
+
+    return False
 
 
 def list_clients():
-    global clients
+    for idx, client in enumerate(clients):
+        print("{}: {}".format(idx, client))
 
-    print(clients)
-
-
-def _add_comma():
-    global clients
-
-    clients += ", "
 
 
 def _print_welcome():
@@ -59,6 +52,7 @@ def _print_welcome():
     print("[U]pdate client")
     print("[D]elete client")
     print("[S]earch client")
+    print("[L]ist clients")
 
 
 def _get_client_name():
@@ -97,9 +91,7 @@ if __name__ == "__main__":
         update_client_name = input("What is the new name of the client? ")
         update_client(client_name, update_client_name)
         list_clients()
+    elif command == "L":
+        list_clients()
     else:
         print("Invalid command")
-
-    create_client("Maria")
-
-    list_clients()
